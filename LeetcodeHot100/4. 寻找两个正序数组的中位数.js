2744d97db93e7d6d888c62d163d73bee209b220e
“huangdaohong” <2224397297@qq.com>
2023-03-04 14:52:49 +0800

    4

--- LeetcodeHot100/4. 寻找两个正序数组的中位数.js
+++ LeetcodeHot100/4. 寻找两个正序数组的中位数.js
@@ -2,4 +2,4 @@ var findMedianSortedArrays = function (nums1, nums2) {
   let arr = [...nums1, ...nums2].sort((a, b) => a - b);
   return arr.length & 1 ? arr[(arr.length - 1) / 2] : (arr[arr.length / 2] + arr[(arr.length - 2) / 2]) / 2;
 };
-//  arr.length & 1 判断奇偶
+//arr.length & 1 判断奇偶
