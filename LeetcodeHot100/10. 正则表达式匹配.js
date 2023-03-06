876714ab0c5e2a71bee0977655d0e9a9aa80156d
“huangdaohong” <2224397297@qq.com>
2023-03-06 14:12:33 +0800

    te

--- LeetcodeHot100/10. 正则表达式匹配.js
+++ LeetcodeHot100/10. 正则表达式匹配.js
@@ -1,7 +1,2 @@
-/**
- * @param {string} s
- * @param {string} p
- * @return {boolean}
- */
 const isMatch = (s, p) => new RegExp(`^${p}$`).test(s);
 // ^表示开头, $表示结尾, test()方法用于检测一个字符串是否匹配某个模式, 如果字符串中含有与RegExp对象匹配的文本，则返回true，否则返回false
