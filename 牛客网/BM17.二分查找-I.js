1e9f7279d8fa5340924b43cc1c02a383dea36965
3192638245@qq.com <2224397297@qq.com>
2023-04-09 09:07:11 +0800

    111

--- 牛客网/BM17.二分查找-I.js
+++ 牛客网/BM17.二分查找-I.js
@@ -1,10 +1,3 @@
-/**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- *
- * @param nums int整型一维数组
- * @param target int整型
- * @return int整型
- */
 function search (nums, target) {
   // write code here
   let [l, r] = [0, nums.length - 1];
