572fc39185a623ce6c5498610a5d25e68964d9ab
3192638245@qq.com <2224397297@qq.com>
2023-04-13 22:17:14 +0800

    17

--- LeetcodeHot100/17. 电话号码的字母组合.js
+++ LeetcodeHot100/17. 电话号码的字母组合.js
@@ -16,25 +16,25 @@ function backtracking(参数) {
     }
 
 */
-const letterCombinations = digits => {
-  if (!digits) return [];
-  const map = [null, null, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
-  const res = [];
-
-  const backTracking = (path, i) => { // path是路径，i是第几个数字
-    if (i >= digits.length) {
+const letterCombinations = digits => { // 定义函数 letterCombinations，它接受一个字符串参数 digits
+  if (!digits) return []; // 如果 digits 为空，则返回一个空数组
+  const map = [null, null, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']; // 定义一个映射表 map，用于存储数字和字母的对应关系
+  const res = []; // 定义一个空数组 res，用于存储所有字母组合
+  const backTracking = (path, i) => { // 定义一个回溯函数 backTracking，它接受两个参数：当前路径 path 和当前数字的索引 i
+    if (i >= digits.length) { // 如果当前数字的索引超出了 digits 的长度，则说明已经找到了一种字母组合，将其加入 res 数组
       res.push(path);
       return;
     }
-    const letters = map[digits[i]];
-    for (const char of letters) {
-      backTracking(`${path}${char}`, i + 1);
+    const letters = map[digits[i]]; // 根据当前数字，从映射表中获取对应的字母集合
+    for (const char of letters) { // 遍历字母集合中的每一个字母
+      backTracking(`${path}${char}`, i + 1); // 将当前字母加入路径中，递归调用回溯函数，继续查找下一个数字对应的字母集合
     }
   };
 
-  backTracking('', 0);
-  return res;
+  backTracking('', 0); // 调用回溯函数，开始查找所有字母组合
+  return res; // 返回包含所有字母组合的数组
 };
+
 
 var letterCombinations1 = function (digits) {
   const k = digits.length;
@@ -64,4 +64,4 @@ var letterCombinations1 = function (digits) {
 };
 
 // 测试
-console.log(letterCombinations1('234'));
+console.log(letterCombinations('23'));
